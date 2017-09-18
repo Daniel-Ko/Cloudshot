8228b765417588c728f2b2d3134c7ab3f3575304
Jerm <southon55@gmail.com>
2017-09-18 14:07:25 +1200

    Player and moves

--- core/src/game/Main.java
+++ core/src/game/Main.java
@@ -9,10 +9,11 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 public class Main extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
-	
+	Player p;
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
+		p = new Player();
 		//img = new Texture("badlogic.jpg");
 	}
 
@@ -21,7 +22,8 @@ public class Main extends ApplicationAdapter {
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		//batch.draw(img, 0, 0);
+		batch.draw(p.img,p.x, p.y);
+		p.update();
 		batch.end();
 	}
 	
--- core/src/game/Player.java
+++ core/src/game/Player.java
@@ -1,8 +1,20 @@
 package game;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
+
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-public class Player {
 
+public class Player {
+	Texture img;
+	int x;
+	int y;
+	public Player(){
+		img = new Texture("badlogic.jpg");
+	}
+	
+	public void draw(){
+		
+	}
+	public void update(){
+		x++;
+	}
 }
