823212c4ab5a1c503f0ca80e6f487d10962214cb
herdsothom <herdsothom@myvuw.ac.nz>
2017-09-18 19:13:38 +1200

    created controller and set up simple game where ant can be moved around screen

--- core/src/controller/Controller.java
+++ core/src/controller/Controller.java
@@ -1,5 +1,59 @@
 package controller;
 
-public class Controller {
+import com.badlogic.gdx.Input.Keys;
+import com.badlogic.gdx.InputAdapter;
+import com.badlogic.gdx.InputProcessor;
+
+import game.Main;
+
+/**
+ * Extends InputAdapter rather than InputProcessor so that we can just override the methods we need,
+ * rather than having to implement all of them.
+ * This Controller is set as the input processor in the create() function in Main.java. It gets
+ * pinged when an event happens. 
+ * @author tomherdson
+ *
+ */
+public class Controller extends InputAdapter {
+	
+	Main main; // store a reference to main so we can update x/y positions
+	//Main will probably be replaced with a reference to main gamemodel class.
+	
+	public Controller(Main main) {
+		this.main = main;
+	}
+
+	
+	public boolean keyDown(int keycode) {
+		// TODO Auto-generated method stub
+		switch(keycode) {
+		case Keys.A: {
+			main.x-=50;
+			break;
+		}
+		case Keys.D: {
+			main.x+=50;
+			break;
+		}
+		case Keys.W: {
+			main.y+=50;
+			break;
+		}
+		case Keys.S: {
+			main.y-=50;
+			break;
+		}
+		}
+		return true;
+	}
+
+	
+	public boolean keyUp(int keycode) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	
+	
+
 
 }
--- core/src/game/Main.java
+++ core/src/game/Main.java
@@ -2,20 +2,27 @@ package game;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+import controller.Controller;
 
 public class Main extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
 	
-	int x = 50;
+	public int x = 50;
+	public int y = 50;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		InputProcessor controller = (InputProcessor) new Controller(this);
+		Gdx.input.setInputProcessor(controller);//set the controller to recieve input when keys pressed
+		img = new Texture(Gdx.files.internal("anticon.png"));
+		
 	}
 
 	@Override
@@ -23,10 +30,7 @@ public class Main extends ApplicationAdapter {
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		int y = 200;
-		x = Gdx.input.getX();
-		y = Gdx.input.getY();
-		System.out.println(Gdx.graphics.getHeight());
+		
 		
 		
 		batch.draw(img, x, y);
