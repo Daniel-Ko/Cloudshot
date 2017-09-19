1bb0df7d3822aa68854e34c2e4c6f9509d3455e8
Jake <Jake@192.168.1.82>
2017-09-19 17:21:04 +1200

    making constructors for collectables

--- core/src/model/collectable/AbstractBuff.java
+++ core/src/model/collectable/AbstractBuff.java
@@ -1,11 +1,18 @@
 package model.collectable;
 
+import com.badlogic.gdx.math.Vector2;
+
 import model.GameObjectInterface;
 import view.SpriteDrawer;
 
 public abstract class AbstractBuff extends AbstractCollectable {
 
-    @Override
+    public AbstractBuff(Vector2 position, int width, int height) {
+		super(position, width, height);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
     public int getX() {
         return 0;
     }
