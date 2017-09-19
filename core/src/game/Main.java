09140f637ce591a7bbf285aa5ee8e243c83320fe
Jerm <southon55@gmail.com>
2017-09-19 15:16:41 +1200

    abstract player filled

--- core/src/game/Main.java
+++ core/src/game/Main.java
@@ -7,24 +7,25 @@ import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
 
 import controller.Controller;
+import controller.PlayerController;
+import model.being.Player;
 
 public class Main extends ApplicationAdapter {
 	Controller controller;
-	
+	PlayerController pcontroller;
+	Player p ;
 	SpriteBatch batch;
 	Texture img;
-	
-	public int x = 50;
-	public int y = 50;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		controller = new Controller(this);
-		Gdx.input.setInputProcessor(controller);//set the controller to recieve input when keys pressed
-		img = new Texture(Gdx.files.internal("anticon.png"));
+		p = new Player(new Vector2(50,160), 100, 100, 10, new Vector2(3,3), new Texture(Gdx.files.internal("anticon.png")));
+		pcontroller= new PlayerController(p);
+		Gdx.input.setInputProcessor(pcontroller);//set the controller to recieve input when keys pressed
 		
 	}
 
@@ -32,31 +33,19 @@ public class Main extends ApplicationAdapter {
 	public void render () {
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+		//Updating game entities
+		updatePlayer();
+		
+		//Displaying game
 		batch.begin();
-		
-		if(controller.keyPressed) {
-			switch(controller.keycode) {
-			case Keys.A: {
-				this.x-=10;
-				break;
-			}
-			case Keys.D: {
-				this.x+=10;
-				break;
-			}
-			case Keys.W: {
-				this.y+=10;
-				break;
-			}
-			case Keys.S: {
-				this.y-=10;
-				break;
-			}
-			}
-		}
-		
-		batch.draw(img, x, y);
+		batch.draw(p.getPlayerImage(),p.getPos().x,p.getPos().y);
 		batch.end();
+	}
+	
+	public void updatePlayer(){
+		pcontroller.applyMovement();
+		p.update();
 	}
 	
 	@Override
