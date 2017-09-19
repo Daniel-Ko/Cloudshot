f04a167d763dfc50d7186801b30bf3f5d23ce6dd
Yi Sian Lim <limyisian@gmail.com>
2017-09-19 16:25:00 +1200

    Added camera

---
+++ core/src/view/Camera.java
@@ -0,0 +1,9 @@
+package view;
+
+import com.badlogic.gdx.graphics.OrthographicCamera;
+
+public class Camera extends OrthographicCamera {
+
+    private final int WORLD_WIDTH = 100;
+    private final int WORLD_HEIGHT = 100;
+}
--- core/src/view/View.java
+++ core/src/view/View.java
@@ -4,8 +4,11 @@ import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.MathUtils;
 import controller.Controller;
 
 public class View extends ApplicationAdapter{
@@ -21,19 +24,40 @@ public class View extends ApplicationAdapter{
     SpriteDrawer walkingMan;
     float elapsedTime;
 
+    // Map
+    private OrthographicCamera cam;
+    private Sprite mapSprite;
 
     @Override
     public void create () {
         batch = new SpriteBatch();
         controller = new Controller(this);
-        Gdx.input.setInputProcessor(controller);//set the controller to recieve input when keys pressed
+        Gdx.input.setInputProcessor(controller);//set the controller to receive input when keys pressed
         img = new Texture(Gdx.files.internal("anticon.png"));
+
         walkingMan = new SpriteDrawer("sprite-animation4.png", 5, 6);
         walkingMan.createSprite(0.25f);
+
+        mapSprite = new Sprite(new Texture(Gdx.files.internal("game_map.jpg")));
+        mapSprite.setPosition(0, 0);
+        mapSprite.setSize(100, 100);
+
+        float w = Gdx.graphics.getWidth()* 0.5f;
+        float h = Gdx.graphics.getHeight() * 0.5f;
+
+        // Constructs a new OrthographicCamera, using the given viewport width and height
+        // Height is multiplied by aspect ratio.
+        cam = new OrthographicCamera(30, 30 * (h / w));
+        cam.position.set(cam.viewportWidth, cam.viewportHeight, 0);
+        cam.update();
     }
 
     @Override
     public void render () {
+        handleInput();
+        cam.update();
+        batch.setProjectionMatrix(cam.combined);
+
         elapsedTime += Gdx.graphics.getDeltaTime();
 
         Gdx.gl.glClearColor(1, 0, 0, 1);
@@ -61,10 +85,41 @@ public class View extends ApplicationAdapter{
             }
         }
 
-        batch.draw(walkingMan.getFrameFromTime(elapsedTime), 0, 0);
+        batch.draw(mapSprite, 0,0);
+        batch.draw(walkingMan.getFrameFromTime(elapsedTime), 100, 100);
         batch.draw(img, x, y);
         batch.end();
     }
+
+    private void handleInput() {
+        if (Gdx.input.isKeyPressed(Input.Keys.A)) {
+            cam.zoom += 0.1;
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.Q)) {
+            cam.zoom -= 0.1;
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
+            cam.translate(-10, 0, 0);
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
+            cam.translate(10, 0, 0);
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.DOWN)) {
+            cam.translate(0, -3, 0);
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.UP)) {
+            cam.translate(0, 3, 0);
+        }
+
+        //cam.zoom = MathUtils.clamp(cam.zoom, 0.1f, 100/cam.viewportWidth);
+
+        float effectiveViewportWidth = cam.viewportWidth * cam.zoom;
+        float effectiveViewportHeight = cam.viewportHeight * cam.zoom;
+
+        //cam.position.x = MathUtils.clamp(cam.position.x, effectiveViewportWidth / 2f, 100 - effectiveViewportWidth / 2f);
+        //cam.position.y = MathUtils.clamp(cam.position.y, effectiveViewportHeight / 2f, 100 - effectiveViewportHeight / 2f);
+    }
+
 
     @Override
     public void dispose () {
