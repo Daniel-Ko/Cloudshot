5068e8e1346af4af04b7bb9357b687d6dffa0032
Dan Ko <daniel.j.ko@gmail.com>
2017-09-19 16:18:35 +1200

    Changed Desktop app width/height to be 16:9, 1280x740

--- desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -8,6 +8,8 @@ import view.View;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width = 1280;
+		config.height = 740;
 		new LwjglApplication(new View(), config);
 	}
 }
