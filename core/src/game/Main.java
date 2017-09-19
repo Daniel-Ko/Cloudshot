2e19744dc611adfc668d2c130007f07d000dbae0
herdsothom <herdsothom@myvuw.ac.nz>
2017-09-19 13:43:49 +1200

    solving merge conflict

--- core/src/game/Main.java
+++ core/src/game/Main.java
@@ -60,7 +60,7 @@ public class Main extends ApplicationAdapter {
 	public void render () {
 		elapsedTime += Gdx.graphics.getDeltaTime();
 
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClearColor(1, 1, 1, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
 		
