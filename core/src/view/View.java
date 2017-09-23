27c15d199659196537cede99a819ae45aa896c7a
Yi Sian Lim <limyisian@gmail.com>
2017-09-23 12:28:26 +1200

    Added methods to flip the textures

--- core/src/view/View.java
+++ core/src/view/View.java
@@ -26,11 +26,19 @@ public class View extends ApplicationAdapter{
     private final int WORLD_HEIGHT = 1000;
     private final int WORLD_WIDTH = 2000;
 
+    public static final float FRAME_RATE = 0.09f;
+
+    /**
+     * Controllers.
+     */
     Controller controller;
     PlayerController playerController;//TODO move this into master controller class
+
+    /**
+     * Model of the games.
+     */
     Player player;
     MeleeEnemy e1;
-
     AbstractLevel level;
 
     public int x = 50;
@@ -38,23 +46,28 @@ public class View extends ApplicationAdapter{
 
     private final int viewWidth = 1000;
 
-    // CustomSprite animation.
-    SpriteBatch batch;
+    /**
+     * batch describes many rectangles for the same texture and send to the GPU all at once.
+     */
+    private SpriteBatch batch;
 
-    MovingSprite playerSprite;
-    StaticSprite backgroundImage;
-    MovingSprite enemy1Sprite;
-
-    float elapsedTime;
-
-    // Map
-    private OrthographicCamera cam;
-
-    // Ground textures.
+    /**
+     * UI elements for the game.
+     */
+    private MovingSprite playerSprite;
+    private StaticSprite backgroundImage;
+    private MovingSprite enemy1Sprite;
     private List<CustomSprite> groundSprites;
 
-    public static final float FRAME_RATE = 0.09f;
+    /**
+     * Time elapsed so far. This is used for animation.
+     */
+    private float elapsedTime;
 
+    /**
+     * Camera in the view. At the moment, the camera follows the player.
+     */
+    private OrthographicCamera cam;
 
     @Override
     public void create () {
@@ -66,29 +79,24 @@ public class View extends ApplicationAdapter{
 
         level = new LevelOne();
 
-        Gdx.input.setInputProcessor(player);//set the controller to receive input when keys pressed
+        // Set the controller to receive input when keys pressed.
+        Gdx.input.setInputProcessor(player);
 
+        // Initialise the sprites.
         enemy1Sprite = e1.getImage();
         playerSprite = player.getImage();
-
         backgroundImage = new StaticSprite("background.png",WORLD_WIDTH,WORLD_HEIGHT);
-
-
         groundSprites = new ArrayList<>();
         for(AbstractTerrain t : level.getTerrain()){
             CustomSprite customSprite = t.getImage();
             groundSprites.add(customSprite);
         }
 
-        float w = Gdx.graphics.getWidth();//* 0.5f;
-        float h = Gdx.graphics.getHeight();// * 0.5f;
-
-
+        float w = Gdx.graphics.getWidth();
+        float h = Gdx.graphics.getHeight();
 
         cam = new OrthographicCamera(viewWidth,viewWidth * (h / w));
-       cam.position.set(cam.viewportWidth / 2f, cam.viewportHeight / 2f, 0);
-        //cam.position.set(300f,300f,0f);
-        //cam.zoom = 1000f;
+        cam.position.set(cam.viewportWidth / 2f, cam.viewportHeight / 2f, 0);
         cam.update();
     }
 
@@ -101,8 +109,10 @@ public class View extends ApplicationAdapter{
         //Updating player model
         playerController.applyMovement();
         player.update();
+
         //for all enemies check if player is hitting them
         player.attack(e1);
+
         //re-updating players image based on state
         playerSprite = player.getImage();
         enemy1Sprite = e1.getImage();
@@ -137,9 +147,6 @@ public class View extends ApplicationAdapter{
     }
 
     private void updateCamera() {
-
-        //cam.zoom = MathUtils.clamp(cam.zoom, 1000f, 1000/cam.viewportWidth);
-
         float effectiveViewportWidth = cam.viewportWidth * cam.zoom;
         float effectiveViewportHeight = cam.viewportHeight * cam.zoom;
 
@@ -153,13 +160,12 @@ public class View extends ApplicationAdapter{
 
         cam.position.set(player.getX(),cam.position.y,0);
         cam.position.x = MathUtils.clamp(cam.position.x, effectiveViewportWidth / 2f, WORLD_WIDTH - effectiveViewportWidth);
-        cam.position.y = MathUtils.clamp(cam.position.y, effectiveViewportHeight / 2f, 1000 - effectiveViewportHeight / 2f);
+        cam.position.y = MathUtils.clamp(cam.position.y, effectiveViewportHeight / 2f, WORLD_HEIGHT - effectiveViewportHeight / 2f);
 
     }
 
     @Override
     public void dispose () {
         batch.dispose();
-        //img.dispose();
     }
 }
