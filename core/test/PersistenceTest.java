db8ce2a784e19d792633fa3c5eb369254066b093
kodani <kodani@myvuw.ac.nz>
2017-10-16 15:25:05 +1300

    Test building for Persistence

--- core/test/PersistenceTest.java
+++ core/test/PersistenceTest.java
@@ -1,41 +1,91 @@
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.World;
+import mock.MockLevel;
+import mock.MockModelData;
+import mock.MockSaveLoader;
 import model.being.enemies.AbstractEnemy;
 import model.being.enemies.Rogue;
 import model.being.player.AbstractPlayer;
 import model.being.player.Player;
+import model.being.player.PlayerData;
 import model.collectable.AbstractCollectable;
 import model.data.GameStateTransactionHandler;
 import model.data.ModelData;
+import model.mapObject.levels.AbstractLevel;
+import model.mapObject.levels.LevelOne;
 import model.mapObject.levels.Spawn;
 import org.junit.Test;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import static junit.framework.TestCase.assertTrue;
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.fail;
 
 /**
  * Created by kodani on 28/09/17.
  */
 public class PersistenceTest extends GameTest{
 
+    //
+    //  NULL CHECKS
+    //
+
     @Test
     public void testSavingPlayerNull() {
-        GameStateTransactionHandler nullCatcher = new GameStateTransactionHandler();
+        MockSaveLoader nullCatcher = new MockSaveLoader();
 
         assertFalse(nullCatcher.validateAndUpdatePlayer(null, null));
     }
 
     @Test
-    public void popEmptyStack() {
+    public void testSavingEnemiesNull() {
+        MockSaveLoader nullCatcher = new MockSaveLoader();
 
+        assertFalse(nullCatcher.validateAndUpdateEnemies(null, null));
     }
 
     @Test
+    public void testSavingLevelNull() {
+        MockSaveLoader nullCatcher = new MockSaveLoader();
+
+        assertFalse(nullCatcher.validateAndUpdateLevel(null, null));
+    }
+
+    //
+    //  TRANSACTION STATE CHECKS
+    //
+
+    @Test
+    public void repoStartsAsEmpty() {
+        MockSaveLoader emptyRepoChecker = new MockSaveLoader();
+
+        // Repo should be empty upon initialisation
+        assertTrue(emptyRepoChecker.repository.latestSaveNum() == 0);
+    }
+
+    @Test
+    public void loadFromEmptyRepoFails() {
+        MockSaveLoader emptyRepoLoader = new MockSaveLoader();
+
+        try {
+            emptyRepoLoader.load();
+            fail();
+
+        } catch (GameStateTransactionHandler.InvalidTransactionException e) {
+
+        }
+    }
+
+    //
+    //  VALID SAVES
+    //
+
+    @Test
     public void testValidSavePerson() {
-        GameStateTransactionHandler nullCatcher = new GameStateTransactionHandler();
+        MockSaveLoader nullCatcher = new MockSaveLoader();
         assertFalse(false);
 
         //assertFalse(nullCatcher.validateAndUpdatePlayer(null
@@ -43,19 +93,27 @@ public class PersistenceTest extends GameTest{
 
     @Test
     public void testValidSave() {
-        GameStateTransactionHandler repoScraper = new GameStateTransactionHandler();
+        MockSaveLoader repoScraper = new MockSaveLoader();
+
+        // Repo should be empty upon initialisation
+        assertTrue(repoScraper.repository.latestSaveNum() == 0);
 
         AbstractPlayer pl = new Player();
         List<AbstractEnemy> enems = new ArrayList<>();
-        List<AbstractCollectable> collects = new ArrayList<>();
-        List<Rectangle> spawnTrigs = new ArrayList<>();
-        List<Spawn> spawns = new ArrayList<>();
+        MockLevel level = new MockLevel();
 
         enems.add(new Rogue(new World(new Vector2(0, -8), true), pl, new Vector2(15, 15)));
 
-//        repoScraper.save(new ModelData(
-//                pl, enems, collects, spawnTrigs, spawns
-//        ));
+        MockModelData data = new MockModelData();
+
+        data.setPlayerData(new PlayerData(pl));
+        data.setEnemies(enems);
+        data.setLevel(level);
+
+        repoScraper.save(data);
+
+        // Unit of Work should have worked and saved 1 GameState to the repo
+        assertTrue(repoScraper.repository.latestSaveNum() == 1);
     }
 
 
--- core/test/mock/MockSaveLoader.java
+++ core/test/mock/MockSaveLoader.java
@@ -1,98 +1,98 @@
-package model.data;
+package mock;
 
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.math.Rectangle;
-import model.GameObjectInterface;
 import model.being.enemies.AbstractEnemy;
 import model.being.player.AbstractPlayer;
 import model.being.player.PlayerData;
-import model.collectable.AbstractCollectable;
-import model.mapObject.levels.AbstractLevel;
-import model.mapObject.levels.Spawn;
+import model.data.GameState;
+import model.data.GameStateRepository;
+import model.data.GameStateTransactionHandler;
+
 
 import java.io.*;
 import java.util.Base64;
 import java.util.List;
 
-
 /**
- * Created by Dan Ko on 9/19/2017.
+ * Mock GameStateTransactionHandler class for testing, with all fields and methods as public
+ *
+ * Created by kodani on 16/10/17.
  */
-public class GameStateTransactionHandler {
-    private GameStateRepository repository;
-    
-    public GameStateTransactionHandler() {
+public class MockSaveLoader {
+    public GameStateRepository repository;
+
+    public MockSaveLoader() {
         repository = new GameStateRepository();
     }
-    
-    public void save(ModelData model) {
+
+    public void save(MockModelData model) {
         GameState newState = new GameState(
                 Gdx.app.getPreferences(
                         "save" + repository.latestSaveNum() //always a unique name and should stay dynamic + consistent throughout repo operations
                 ));
         try {
             writeQuery(model, newState);
-        } catch(InvalidTransactionException e) {
-            throw new InvalidTransactionException(e.getMessage());
+        } catch(GameStateTransactionHandler.InvalidTransactionException e) {
+            throw new GameStateTransactionHandler.InvalidTransactionException(e.getMessage());
         }
 
         // Unit of work complete, we now commit the gamestate to the repository
         commit(newState);
     }
-    
-    private void writeQuery(ModelData model, GameState newState) {
+
+    private void writeQuery(MockModelData model, GameState newState) {
 
         /* spawnEnemies the newState with validated data, otherwise signal failed save */
         try {
             validateAndUpdatePlayer(newState, model.loadPlayer());
             validateAndUpdateEnemies(newState, model.loadEnemies());
             validateAndUpdateLevel(newState, model.loadLevel());
-        } catch(InvalidTransactionException e) {
-            throw new InvalidTransactionException(e.getMessage());
+        } catch(GameStateTransactionHandler.InvalidTransactionException e) {
+            throw new GameStateTransactionHandler.InvalidTransactionException(e.getMessage());
         }
     }
-    
-    
+
+
     /**
      * sends signal to repo to pull the latestSaveNum state and wrap it in a StateQuery to the model
      *
      * @return
      */
-    public ModelData load() throws InvalidTransactionException {
+    public MockModelData load() throws GameStateTransactionHandler.InvalidTransactionException {
         GameState latest = repository.pullSoft();
         if (latest == null)
-            throw new InvalidTransactionException("No saved games!");
-        
+            throw new GameStateTransactionHandler.InvalidTransactionException("No saved games!");
+
         if (!latest.containsPlayer() || !latest.containsEnemies()) {
             repository.pullHard(); //cleanse the repo of the bad state
-            throw new InvalidTransactionException("Corrupted save");
+            throw new GameStateTransactionHandler.InvalidTransactionException("Corrupted save");
         }
-        
+
         try {
-            
+
             //if all data is valid, remove it from stack finally
-            ModelData loadedData = new ModelData();
+            MockModelData loadedData = new MockModelData();
             loadedData.setPlayerData(validateAndReturnPlayerData(latest));
             loadedData.setEnemies(validateAndReturnEnemies(latest));
             loadedData.setLevel(validateAndReturnLevel(latest));
-            
+
             //unit of work complete, we now commit the change permanently
             repository.pullHard();
-            
+
             return loadedData; //give the model a loader object to directly call validated data
-            
-        } catch (InvalidTransactionException e) {
+
+        } catch (GameStateTransactionHandler.InvalidTransactionException e) {
             repository.pullHard(); //remove corrupted data
-            throw new InvalidTransactionException(e.getMessage());
+            throw new GameStateTransactionHandler.InvalidTransactionException(e.getMessage());
         }
     }
-    
-    
+
+
     private void commit(GameState newState) {
         repository.push(newState);
     }
-    
-    
+
+
     /**
      * Convert the AbstractPlayer obtained from the model into a serialisable PlayerData object and store it
      * into the buffer GameState. Checks if the data to be stored exists in the right type
@@ -103,29 +103,29 @@ public class GameStateTransactionHandler {
      * @return
      */
     @SuppressWarnings("Duplicates")
-    private boolean validateAndUpdatePlayer(GameState newState, AbstractPlayer newPlayer) {
+    public boolean validateAndUpdatePlayer(GameState newState, AbstractPlayer newPlayer) {
         if (newPlayer == null) {
             return false;
         }
-        
+
         //create a PlayerData object that makes serializable objects out of a not-entirely serializable AbstractPlayer
         //, particularly Box2D.
         PlayerData playerProps = new PlayerData(newPlayer);
-        
+
         //now serialise the PlayerData object and add to Preferences
-        
+
         String playerSer = "";
         try {
             playerSer = serializeInBase64(playerProps);
-            
+
             newState.setPlayerInPref(playerSer);
             return true;
-            
+
         } catch (IOException e) {
-            throw new InvalidTransactionException(e.getMessage());
+            throw new GameStateTransactionHandler.InvalidTransactionException(e.getMessage());
         }
     }
-    
+
     /**
      * Serialise the List of enemies obtained from the model and store it
      * into the buffer GameState. Checks if the data to be stored exists in the right type
@@ -136,44 +136,44 @@ public class GameStateTransactionHandler {
      * @return
      */
     @SuppressWarnings("Duplicates")
-    private boolean validateAndUpdateEnemies(GameState newState, List<AbstractEnemy> newFoes) {
+    public boolean validateAndUpdateEnemies(GameState newState, List<AbstractEnemy> newFoes) {
         if (newFoes == null) {
             return false;
         }
-        
+
         //now serialise the Enemies List and add to Preferences
         String enemiesSer = "";
         try {
             enemiesSer = serializeInBase64(newFoes);
-            
+
             newState.setEnemiesInPref(enemiesSer);
             return true;
-            
+
         } catch (IOException e) {
-            throw new InvalidTransactionException(e.getMessage());
+            throw new GameStateTransactionHandler.InvalidTransactionException(e.getMessage());
         }
     }
-    
-    
-    private boolean validateAndUpdateLevel(GameState newState, AbstractLevel newLevel) {
+
+
+    public boolean validateAndUpdateLevel(GameState newState, MockLevel newLevel) {
         if (newLevel == null) {
             return false;
         }
-        
+
         //now serialise the Enemies List and add to Preferences
         String levelSer = "";
         try {
             levelSer = serializeInBase64(newLevel);
-            
+
             newState.setLevelInPref(levelSer);
             return true;
-            
+
         } catch (IOException e) {
-            throw new InvalidTransactionException(e.getMessage());
+            throw new GameStateTransactionHandler.InvalidTransactionException(e.getMessage());
         }
     }
-    
-    
+
+
     /**
      * Deserialize the Player's data from the GameState and validate that the data is correct
      *
@@ -181,55 +181,55 @@ public class GameStateTransactionHandler {
      * @return validated PlayerData to be loaded into a StateQuery and transferred into a new
      * AbstractPlayer instance
      */
-    private PlayerData validateAndReturnPlayerData(GameState latest) throws InvalidTransactionException {
+    public PlayerData validateAndReturnPlayerData(GameState latest) throws GameStateTransactionHandler.InvalidTransactionException {
         try {
             Object p = deserializeFromBase64(latest.getPref().getString("Player"));
             if (!(p instanceof PlayerData))
-                throw new InvalidTransactionException("Deserialized player object isn't a PlayerData");
-            
+                throw new GameStateTransactionHandler.InvalidTransactionException("Deserialized player object isn't a PlayerData");
+
             return (PlayerData) p;
         } catch (IOException | ClassNotFoundException e) {
-            throw new InvalidTransactionException(e.getMessage());
+            throw new GameStateTransactionHandler.InvalidTransactionException(e.getMessage());
         }
     }
-    
+
     /**
      * Deserialize the List of enemies from the GameState and validate that the data is correct
      *
      * @param latest
      * @return validated List of AbstractEnemies to be loaded into model
      */
-    private List<AbstractEnemy> validateAndReturnEnemies(GameState latest) throws InvalidTransactionException {
+    public List<AbstractEnemy> validateAndReturnEnemies(GameState latest) throws GameStateTransactionHandler.InvalidTransactionException {
         try {
             Object e = deserializeFromBase64(latest.getPref().getString("Enemies"));
-            
+
             if (!(e instanceof List))
-                throw new InvalidTransactionException("Deserialized enemies object isn't a List");
-            
+                throw new GameStateTransactionHandler.InvalidTransactionException("Deserialized enemies object isn't a List");
+
             @SuppressWarnings("unchecked") List<AbstractEnemy> enemies = (List<AbstractEnemy>) e;
-            
+
             for (int i = 0; i < enemies.size(); i++) {
                 if (!(enemies.get(i) instanceof AbstractEnemy))
-                    throw new InvalidTransactionException("Deserialized enemy list doesn't contain AbstractEnemies");
+                    throw new GameStateTransactionHandler.InvalidTransactionException("Deserialized enemy list doesn't contain AbstractEnemies");
             }
-            
+
             return enemies;
         } catch (IOException | ClassNotFoundException e) {
-            throw new InvalidTransactionException(e.getMessage());
+            throw new GameStateTransactionHandler.InvalidTransactionException(e.getMessage());
         }
     }
-    
-    private AbstractLevel validateAndReturnLevel(GameState latest) throws InvalidTransactionException {
+
+    public MockLevel validateAndReturnLevel(GameState latest) throws GameStateTransactionHandler.InvalidTransactionException {
         try {
             Object l = deserializeFromBase64(latest.getPref().getString("Level"));
-            
-            if (!(l instanceof AbstractLevel))
-                throw new InvalidTransactionException("Deserialized Level object isn't an AbstractLevel");
-            
-            return (AbstractLevel) l;
-            
+
+            if (!(l instanceof MockLevel))
+                throw new GameStateTransactionHandler.InvalidTransactionException("Deserialized Level object isn't an MockLevel");
+
+            return (MockLevel) l;
+
         } catch (IOException | ClassNotFoundException e) {
-            throw new InvalidTransactionException(e.getMessage());
+            throw new GameStateTransactionHandler.InvalidTransactionException(e.getMessage());
         }
     }
 
@@ -238,8 +238,8 @@ public class GameStateTransactionHandler {
        ============================= SERIALIZATION ============================================
        ========================================================================================
      */
-    
-    
+
+
     /**
      * Serialise an object and return its bytecode String
      *
@@ -247,7 +247,7 @@ public class GameStateTransactionHandler {
      * @return
      * @throws IOException
      */
-    private String serializeInBase64(Object o) throws IOException {
+    public String serializeInBase64(Object o) throws IOException {
         String ser = "";
         try {
             //serialize our object
@@ -255,24 +255,24 @@ public class GameStateTransactionHandler {
             ObjectOutputStream so = new ObjectOutputStream(bo);
             so.writeObject(o);
             so.flush(); //persist
-            
+
             ser = new String(Base64.getEncoder().encode(bo.toByteArray())); //convert the bytecode to a character String
-            
+
             return ser;
         } catch (IOException e) {
             throw new IOException(e.getMessage());
         }
     }
-    
-    
-    private Object deserializeFromBase64(String ser) throws IOException, ClassNotFoundException {
+
+
+    public Object deserializeFromBase64(String ser) throws IOException, ClassNotFoundException {
         try {
             byte b[] = Base64.getDecoder().decode(ser.getBytes()); //decode the Base64 string into bytecode
-            
+
             //deserialize the bytecode back into an Object
             ByteArrayInputStream bi = new ByteArrayInputStream(b);
             ObjectInputStream si = new ObjectInputStream(bi);
-            
+
             return si.readObject(); //return the object (will be casted in the parent method)
         } catch (IOException | ClassNotFoundException e) {
             e.printStackTrace();
@@ -280,17 +280,6 @@ public class GameStateTransactionHandler {
                 throw new IOException(e.getMessage());
             else
                 throw new ClassNotFoundException();
-        }
-    }
-    
-    
-    /**
-     * Upon invalid or missing data, this exception will be thrown to rollback all changes
-     * done upon the database
-     */
-    public static class InvalidTransactionException extends RuntimeException {
-        public InvalidTransactionException(String msg) {
-            super(msg);
         }
     }
 }
