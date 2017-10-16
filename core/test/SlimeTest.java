2baa9ed63c5a9063ae2df00b5da3b7c6c7244b4d
Jerm <southon55@gmail.com>
2017-10-16 22:18:57 +1300

    More Player tests

--- core/test/SlimeTest.java
+++ core/test/SlimeTest.java
@@ -14,7 +14,10 @@ import static junit.framework.TestCase.assertFalse;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
-//import org.junit.jupiter.api.Test;
+/**
+ *
+ * @author Jeremy Southon
+ * */
 
 public class SlimeTest extends GameTest {
 
